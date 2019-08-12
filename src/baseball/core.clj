5188ec633974ed2fdc0e1db12672372588378bd4
Bob Follek <bfollek@gmail.com>
2019-08-11 21:01:12 -0400

    Made funcs private for now. Wrote some tests. ATP

--- src/baseball/core.clj
+++ src/baseball/core.clj
@@ -5,19 +5,18 @@
             [hickory.core :as hi]
             [hickory.select :as his]))
 
-(defn game-day-url
+(defn- game-day-url
   [level year month day]
   (str "http://gd2.mlb.com/components/game/" level "/year_" year "/month_" month "/day_" day))
-(comment (slurp (game-day-url "mlb" "2018" "06" "10")))
 
-(defn parse-html
+(defn- parse-html
   [url]
   (-> url
       slurp
       hi/parse
       hi/as-hickory))
 
-(defn game-day-links
+(defn- game-day-links
   [url]
   (->> url
        parse-html
@@ -26,4 +25,5 @@
        (map first) ; Pull string out of vector
        (filter #(str/includes? %1 "gid_"))
        (map str/trim)
-       (map #(str url "/" %1))))
+       (map #(str url "/" %1))
+       sort))
--- test/baseball/core_test.clj
+++ test/baseball/core_test.clj
@@ -2,6 +2,17 @@
   (:require [clojure.test :refer :all]
             [baseball.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def ^{:private true} known-url "http://gd2.mlb.com/components/game/mlb/year_2018/month_06/day_10")
+
+(deftest game-day-url-test
+  (testing "game-day-url"
+    (let [f #'baseball.core/game-day-url]
+      (is (= known-url (f "mlb" "2018" "06" "10"))))))
+
+(deftest game-day-links-test
+  (testing "game-day-links"
+    (let [f #'baseball.core/game-day-links
+          result (f known-url)]
+      (is (= 15 (count result)))
+      (is (= "http://gd2.mlb.com/components/game/mlb/year_2018/month_06/day_10/gid_2018_06_10_anamlb_minmlb_1/" (first result)))
+      (is (= "http://gd2.mlb.com/components/game/mlb/year_2018/month_06/day_10/gid_2018_06_10_slnmlb_cinmlb_1/" (last result))))))
