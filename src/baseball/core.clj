8562dfd8a7615f9b3f66b24cd7fb1580a45780ef
Bob Follek <bfollek@gmail.com>
2019-08-12 09:20:55 -0400

    Started linescore xml

--- src/baseball/core.clj
+++ src/baseball/core.clj
@@ -2,6 +2,7 @@
 
 (ns baseball.core
   (:require [clojure.string :as str]
+            [clojure.data.xml :as xml]
             [hickory.core :as hic]
             [hickory.select :as his]))
 
@@ -26,3 +27,11 @@
        (filter #(str/includes? %1 "gid_"))
        (map str/trim)
        (map #(str url "/" %1))))
+
+(defn- linescore-parse
+  ;; http://gd2.mlb.com/components/game/mlb/year_2018/month_06/day_10/gid_2018_06_10_anamlb_minmlb_1/linescore.xml
+  [url]
+  (-> url
+      slurp
+      (str/replace "&" "&amp;") ; Fix invalid ampersands
+      xml/parse-str))
